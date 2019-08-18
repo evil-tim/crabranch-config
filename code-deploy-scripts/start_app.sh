057aa18b97b9b8e2aabf690757c4f6d66b23defa
evil_tim <epsilon_recursor@yahoo.com>
2019-08-18 18:50:34 +0800

    fail2ban implementation

--- code-deploy-scripts/start_app.sh
+++ code-deploy-scripts/start_app.sh
@@ -5,6 +5,11 @@ if [[ -z $(docker network ls | grep crabranch_network) ]]; then
     docker network create crabranch_network
 fi
 
+# configure ecr login
+aws configure set region `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//'`
+$(aws ecr get-login --no-include-email)
+
 # start
 cd /home/ubuntu/crabranch-proxy
+/usr/bin/docker pull 535425158818.dkr.ecr.ap-southeast-1.amazonaws.com/crabranch/fail2ban:latest
 /usr/local/bin/docker-compose up -d
